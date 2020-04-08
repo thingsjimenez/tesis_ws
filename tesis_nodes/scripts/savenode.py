8baea59bab30d86cbdbfe994eb683b9a5bba4f22
Santiago Jiménez <thingsjimenez@gmail.com>
2020-04-07 19:36:20 -0500

    Se agregan codigos hacer bags y waypoints
    * savenode.py es para guardar los topicos del path y el core de los sensores
    
    * waypoints.py es para generar unos puntos guias a donde va llegar por medio del move base

---
+++ tesis_nodes/scripts/savenode.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+import rospy
+import actionlib
+import rosbag
+from nav_msgs.msg import Path
+from nav_msgs.msg import Odometry
+from kobuki_msgs.msg import SensorState
+from geometry_msgs.msg import PoseStamped
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal, MoveBaseActionResult
+
+class GuardarDatos():
+	def __init__(self):
+		rospy.init_node('save_data_node')
+		rospy.loginfo('Se inicia el nodo de guardar datos')
+
+		self.path_pub = rospy.Publisher('/Path_Robot', Path, queue_size=1)
+		self.result_sub = rospy.Subscriber("/move_base/result", MoveBaseActionResult, self.result_cb) 
+		self.goal_sub = rospy.Subscriber('/move_base_simple/goal',PoseStamped, self.goal_cb)
+		self.odom_sub = rospy.Subscriber('/odom', Odometry, self.odom_cb)
+		self.core_sub = rospy.Subscriber('/mobile_base/sensors/core', SensorState, self.core_cb)
+
+		self.path = Path()
+		self.core = SensorState()
+		self.result = 0
+		self.goal = 0
+
+		self.flag_goal = 0
+		self.bag = rosbag.Bag('test{}.bag'.format(self.goal), 'w')
+
+	def odom_cb(self, data):
+		while (self.goal == self.flag_goal):
+			pass
+
+		self.path.header = data.header
+		pose = PoseStamped()
+		pose.header = data.header
+		pose.pose = data.pose.pose
+		self.path.poses.append(pose)
+		self.path_pub.publish(self.path)
+
+		self.bag.write('Path_Robot', self.path)
+		self.bag.write('mobile_base/sensors/core', self.core)
+
+		if (self.result != 0):
+			rospy.loginfo(self.result)
+			self.result = 0
+			self.flag_goal = self.goal
+			self.path.poses = list()
+			self.bag.close()
+			self.bag = rosbag.Bag('test{}.bag'.format(self.goal), 'w')
+
+	def result_cb(self, data):
+		self.result = data.status.text
+
+	def goal_cb(self, data):
+		self.goal = data.header.seq
+
+	def core_cb(self, data):
+		self.core.header.seq = data.header.seq
+		self.core.header.stamp = data.header.stamp
+		self.core.header.frame_id = data.header.frame_id
+		self.core.time_stamp = data.time_stamp
+		self.core.left_encoder = data.left_encoder
+		self.core.right_encoder = data.right_encoder
+		self.core.left_pwm = data.left_pwm
+		self.core.right_pwm = data.right_pwm
+		self.core.buttons = data.buttons
+		self.core.battery = data.battery
+		self.core.current = data.current
+		self.core.over_current = data.over_current
+
+if __name__ == '__main__':
+	data_bag = GuardarDatos()
+	rospy.spin()
+
